ed6856e3cd0874826ceb3cb55d71d3f58636c322
Anitars6 <arodriguez4728@ceuandalucia.es>
2023-12-01 08:34:39 +0100

    Cantante o grupo favorito

---
+++ ED_Ej_1_2/src/HolaMundo/CantanteFavorito.java
@@ -0,0 +1,12 @@
+package HolaMundo;
+
+import java.util.Scanner;
+
+public class CantanteFavorito {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Estopa");
+	}
+
+}
